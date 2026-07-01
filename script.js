633d452a3f9545c61d8a5dcd0d91e151a7293dd9
Atif Sarfraz <atifsarfraz80@gmail.com>
2026-07-01 16:33:06 +0500

    Added the new Grid button. this button takes up the new size upto 100 and then removes previous grid and creates a new one

--- script.js
+++ script.js
@@ -1,9 +1,44 @@
 let container = document.querySelector('.grid');
 
-for(let i = 0 ; i < 256 ; i++)
+function gridCreation(size)
 {
-    const box = document.createElement('div');
-    box.classList.add("grid-box");
+    container.innerHTML = "";
 
+    const boxPercentage = 100/size;
+
+    const totalBoxes = size*size;
+    for(let i = 0 ; i < totalBoxes ; i++)
+    {
+        const box = document.createElement('div');
+        box.classList.add("grid-box");
+
+        box.style.width = `${boxPercentage}%`;
+        box.style.height = `${boxPercentage}%`;
+        box.style.boxSizing = 'border-box';
+        box.style.border = '1px solid black';
+
+        box.addEventListener("mouseenter", (e) =>{
+        e.target.style.backgroundColor = '#b4e1ff';
+    });
     container.appendChild(box);
+    }
 }
+
+gridCreation(16);
+
+
+let button = document.querySelector('.butt');
+button.addEventListener('click' , () => {
+    let size = prompt("Enter the Size of your new Grid.(MAX 100)");
+
+    size = parseInt(size);
+
+    while (isNaN(size) || size < 1 || size > 100)
+    {
+        size = prompt("Enter the Size of your new Grid.(MAX 100)");
+        size = parseInt(size);
+    }
+
+    gridCreation(size);
+
+});
